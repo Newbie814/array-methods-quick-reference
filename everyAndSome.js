b9376a485cc0694f2a190a71444838aca695155b
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-01 13:38:21 -0400

    exercises from previous methods and notes about passing a function into Array.from()

--- everyAndSome.js
+++ everyAndSome.js
@@ -15,27 +15,3 @@ let scores2 = [80, 50, 0, 100, 90, 80, 75];
 let results2 = scores2.some(function (element) {
   return element === 0;
 });
-
-// function returning # of 0s in this array
-let scores3 = [80, 50, 0, 100, 90, 80, 75, 0, 0, 5, 65, 93];
-
-let numZeros = scores3.filter((element) => {
-  return element === 0;
-});
-
-console.log(numZeros.length);
-
-let zeroCt = 0;
-
-scores3.forEach(function (val) {
-  if (val === 0) {
-    zeroCt++;
-  }
-});
-console.log(zeroCt);
-
-let zeroCt2 = scores3.reduce(function (acc, val) {
-  return val === 0 ? acc + 1 : acc;
-}, 0);
-
-console.log(zeroCt2);
