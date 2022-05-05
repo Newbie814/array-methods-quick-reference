6c85439c1036ba6b95f4e8b07426acb6172d288b
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-05 12:38:07 -0400

    added find() and findIndex() section

--- app.js
+++ app.js
@@ -41,4 +41,44 @@ if (students.includes('lita')) {
 // indexOf() and lastIndexOf() and includes() are equivalent to using === match has to match type and value
 // no coercion is applied to the values being compared
 
-// find()
+// find()  is more flexible (==) and returns the value, not the index
+
+let scores2 = ['80', 90, '55', 60, 85, 95, 25, '100'];
+let scores3 = ['80', 90, '55', 100, 60, 85, 95, 25, '100'];
+
+let topScore = scores2.find(function (val) {
+  return val == 100;
+});
+
+console.log(topScore); // '100' returns string value
+
+let topScore2 = scores3.find(function (val) {
+  return val == 100;
+});
+
+console.log(topScore2); // 100 - number
+
+let users = [
+  { name: 'Steve', q1: 45, q2: 85 },
+  { name: 'Ignacio', q1: 100, q2: 100 },
+  { name: 'Alvin', q1: 95, q2: 100 },
+  { name: 'Brianna', q1: 100, q2: 85 },
+  { name: 'Andrea', q1: 75, q2: 95 },
+];
+
+let topScoreBothTests = users.find((val) => val.q1 == 100 && val.q2 == 100);
+
+console.log(topScoreBothTests); // { name: 'Ignacio', q1: 100, q2: 100 }
+
+//findIndex()  provides flexibility, but returns the index
+
+let topScoreBothTests2 = users.findIndex(
+  (val) => val.q1 == 100 && val.q2 == 100
+);
+
+let topScore3 = scores3.findIndex(function (val) {
+  return val == 100;
+}); // identical to above with find replace with findIndex and returning index
+
+console.log(topScoreBothTests2); // 1
+console.log(topScore3); // 3
